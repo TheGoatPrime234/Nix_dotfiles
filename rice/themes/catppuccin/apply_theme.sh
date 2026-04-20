0dc35d01d0ab9598fe53ade9fa6755c99803ea70
TheGoatPrime234 <cato.jenisch@gmail.com>
2026-04-20 14:36:40 +0200

    Catppuccin themechanges

---
+++ rice/themes/catppuccin/apply_theme.sh
@@ -0,0 +1,4 @@
+cp ~/.config/rice/themes/catppuccin/waybar/current.css ~/.config/waybar/current.css
+cp ~/.config/rice/themes/catppuccin/swaync/current.css ~/.config/swaync/current.css
+cp ~/.config/rice/themes/catppuccin/hyprland/color.conf ~/.config/hypr/color.conf
+cp ~/.config/rice/themes/catppuccin/rofi/current.rasi ~/.config/rofi/current.rasi
