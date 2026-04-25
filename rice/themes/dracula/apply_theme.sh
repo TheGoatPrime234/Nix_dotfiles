80e9d65688e2a6e793c021a0554d5236f55003e1
TheGoatPrime234 <cato.jenisch@gmail.com>
2026-04-25 13:05:07 +0200

    Second Commit

--- rice/themes/dracula/apply_theme.sh
+++
@@ -1,5 +0,0 @@
-cp ~/.config/rice/themes/dracula/waybar/current.css ~/.config/waybar/current.css
-cp ~/.config/rice/themes/dracula/swaync/current.css ~/.config/swaync/current.css
-cp ~/.config/rice/themes/dracula/hyprland/color.conf ~/.config/hypr/color.conf
-cp ~/.config/rice/themes/dracula/rofi/current.rasi ~/.config/rofi/current.rasi
-cp ~/.config/rice/themes/dracula/kitty/current.conf ~/.config/kitty/current.conf
